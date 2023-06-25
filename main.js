5c4515344c47118a27c11761ac9fdfdba256f873
Natan Bond <97616282+NatanBondarenko@users.noreply.github.com>
2023-06-25 15:16:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,64 +1,100 @@
 looker.plugins.visualizations.add({
-  create: function (element, config) {
-    // Add the Google Charts loader script
-    var script = document.createElement('script');
-    script.src = 'https://www.gstatic.com/charts/loader.js';
-    document.head.appendChild(script);
+  create: function(element, config) {
+    // Initialize the visualization if required
+  },
 
-    // Create a new <div> element to hold the chart
-    var chartDiv = document.createElement('div');
-    chartDiv.id = 'myChart';
-    chartDiv.style.maxWidth = '700px';
-    chartDiv.style.height = '400px';
+  updateAsync: function(data, element, config, queryResponse, details, done) {
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
 
-    // Append the chart <div> to the main element
-    element.appendChild(chartDiv);
-
-    // Define the callback function to be called when the Google Charts API is loaded
-    window.googleChartsCallback = function () {
-      google.charts.load('current', { packages: ['corechart'] });
-      google.charts.setOnLoadCallback(drawChart);
-    };
-
-    // Define the drawChart function
     function drawChart() {
-      // Get the Looker query result data
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'Dimension');
-      data.addColumn('number', 'Measure');
-
-      // Extract the data from Looker query response
-      var rows = queryResponse['data']['rows'];
-      for (var i = 0; i < rows.length; i++) {
-        var dimension = rows[i]['dimension']['value'];
-        var measure = rows[i]['measure']['value'];
-        data.addRow([dimension, measure]);
+      // Ensure the data fits the requirements
+      if (!handleErrors(this, queryResponse, { 
+        min_pivots: 0, max_pivots: 0, 
+        min_dimensions: 1, max_dimensions: 1, 
+        min_measures: 1, max_measures: 99
+      })) {
+        return;
       }
 
-      // Set the options
+      var dimension = queryResponse.fields.dimension_like[0].name;
+      var measure_count = queryResponse.fields.measure_like.length;
+      var x_label = queryResponse.fields.dimension_like[0].label_short;
+      var series = [];
+      var x = [];
+      var all_series = [];
+
+      for (let i = 0; i < measure_count; i++) {
+        series[i] = {
+          name: queryResponse.fields.measure_like[i].label,
+          field_name: queryResponse.fields.measure_like[i].name,
+          data: [],
+          rendered_data: [],
+          links: []
+        };
+      }
+
+      for (let i = 0; i < data.length; i++) {
+        x.push(data[i][dimension].value);
+        for (let j = 0; j < measure_count; j++) {
+          let datapoint = data[i][series[j].field_name];
+          if (datapoint.links) {
+            series[j].links.push(datapoint.links);
+          }
+          if (config.plot_null && !datapoint.value) {
+            series[j].data.push([x[i], 0]);
+            series[j].rendered_data.push([x[i], 0]);
+          } else {
+            series[j].data.push([x[i], datapoint.value]);
+            series[j].rendered_data.push([x[i], datapoint.rendered]);
+            all_series.push(datapoint.value);
+          }
+        }
+      }
+
+      var minX = Math.min(...all_series) * (1 - config.range_scale);
+      var maxX = Math.max(...all_series) * (1 + config.range_scale);
+
+      var dataTable = new google.visualization.DataTable();
+      dataTable.addColumn('string', x_label);
+      for (let i = 0; i < measure_count; i++) {
+        dataTable.addColumn('number', series[i].name);
+      }
+      dataTable.addRows(x.length);
+      for (let i = 0; i < x.length; i++) {
+        dataTable.setValue(i, 0, x[i]);
+        for (let j = 0; j < measure_count; j++) {
+          dataTable.setValue(i, j + 1, series[j].data[i][1]);
+        }
+      }
+
       var options = {
-        title: 'House Prices vs Size',
-        hAxis: { title: 'Dimension' },
-        vAxis: { title: 'Measure' },
-        legend: 'none'
+        colors: config.color_range,
+        title: null,
+        legend: { position: config.show_legend ? 'top' : 'none' },
+        tooltip: { isHtml: true },
+        hAxis: { title: config.x_axis_label },
+        vAxis: { title: config.y_axis_label, viewWindow: { min: minX, max: maxX } },
+        pointShape: config.point_style === 'none' ? 'circle' : config.point_style,
+        pointSize: 6,
+        series: {
+          0: { pointShape: config.point_style === 'none' ? 'circle' : config.point_style }
+        }
       };
 
-      // Draw the chart
-      var chart = new google.visualization.LineChart(document.getElementById('myChart'));
-      chart.draw(data, options);
+      var chart = new google.visualization.LineChart(document.getElementById('vis'));
+      chart.draw(dataTable, options);
+
+      google.visualization.events.addListener(chart, 'click', function(e) {
+        if (series[e.row].links.length) {
+          LookerCharts.Utils.openDrillMenu({
+            links: series[e.row].links[e.column - 1],
+            event: e
+          });
+        }
+      });
+
+      done();
     }
-
-    // Load the Google Charts API
-    script.onload = function () {
-      google.charts.load('current', { packages: ['corechart'] });
-      google.charts.setOnLoadCallback(window.googleChartsCallback);
-    };
-  },
-  updateAsync: function (data, element, config, queryResponse, details, done) {
-    // Call the drawChart function to update the chart
-    window.googleChartsCallback();
-
-    // Signal the end of the update
-    done();
   }
 });
