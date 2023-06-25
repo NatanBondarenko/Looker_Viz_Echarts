8ca456a8dff6a5ff01b6083a1808bdb3f203321f
Natan Bond <97616282+NatanBondarenko@users.noreply.github.com>
2023-06-25 15:31:42 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
-import { create, Doughnut } from 'chart.js';
+import * as Chart from 'chart.js';
 
 looker.plugins.visualizations.add({
+  options: {
+    my_option: {
+      type: 'string',
+      label: 'My Option',
+      default: 'Default Value',
+    },
+  },
   create: function(element, config) {
     element.innerHTML = '<canvas id="doughnutChart" width="400" height="400"></canvas>';
   },
-  update: function(data, element, config, queryResponse) {
+  updateAsync: function(data, element, config, queryResponse, details, done) {
     const canvas = element.querySelector('#doughnutChart');
     const ctx = canvas.getContext('2d');
 
     const chartData = {
-      labels: data.labels,
-      datasets: [{
-        data: data.values,
-        backgroundColor: data.colors,
-      }]
+      labels: data.fields.dimensions[0].values,
+      datasets: [
+        {
+          data: data.fields.measure_like[0].value,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.7)',
+            'rgba(54, 162, 235, 0.7)',
+            'rgba(255, 206, 86, 0.7)',
+            'rgba(75, 192, 192, 0.7)',
+            'rgba(153, 102, 255, 0.7)',
+          ],
+        },
+      ],
     };
 
     const chartOptions = {
@@ -22,9 +37,12 @@ looker.plugins.visualizations.add({
       // Add more options as needed for customization
     };
 
-    const chart = new Doughnut(ctx, {
+    new Chart(ctx, {
+      type: 'doughnut',
       data: chartData,
-      options: chartOptions
+      options: chartOptions,
     });
-  }
+
+    done();
+  },
 });
