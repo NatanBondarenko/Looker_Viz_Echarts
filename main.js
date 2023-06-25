b6fbe813cfc65c83706fd08ac4ee203534141254
Natan Bond <97616282+NatanBondarenko@users.noreply.github.com>
2023-06-25 13:07:01 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,50 @@
+looker.plugins.visualizations.add({
+  id: 'my-google-chart',
+  label: 'My Google Chart',
+  options: {
+    title: {
+      label: 'Chart Title',
+      type: 'string',
+      default: 'My Chart',
+    },
+    chartType: {
+      label: 'Chart Type',
+      section: 'Style',
+      type: 'string',
+      display: 'select',
+      values: [
+        { 'PieChart': 'Pie Chart' },
+        { 'BarChart': 'Bar Chart' },
+      ],
+      default: 'PieChart',
+    },
+  },
+  handleErrors: function (data, queryResponse) {
+    return [];
+  },
+  create: function (element, config) {
+    element.innerHTML = '<div id="myChart"></div>';
+  },
+  updateAsync: function (data, element, config, queryResponse, details, done) {
+    google.charts.load('current', { packages: ['corechart'] });
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+      var dataTable = new google.visualization.DataTable();
+      // Prepare your data here and populate the `dataTable` object
+
+      var chart;
+      if (config.chartType === 'PieChart') {
+        chart = new google.visualization.PieChart(element.querySelector('#myChart'));
+      } else if (config.chartType === 'BarChart') {
+        chart = new google.visualization.BarChart(element.querySelector('#myChart'));
+      }
+
+      chart.draw(dataTable, {
+        title: config.title,
+      });
+
+      done();
+    }
+  },
+});
