5f34639010d702c746e4d9acf1dada54480a47c5
Natan Bond <97616282+NatanBondarenko@users.noreply.github.com>
2023-06-27 11:52:01 +0300

    Update Heatmap.js

--- Heatmap.js
+++ Heatmap.js
@@ -1,24 +1,85 @@
 looker.plugins.visualizations.add({
-  options: {},
   create: function(element, config) {
     // Create a container element for the chart
-    var container = element.appendChild(document.createElement("div"));
-    container.id = "main";
-    container.style.width = "100%";
-    container.style.height = "100%";
-  },
-  updateAsync: function(data, element, config, queryResponse, details, done) {
-    // Extract the data from the Looker API response
-    const fields = queryResponse.fields;
-    const data = queryResponse.data.map(function(row) {
-      return fields.map(function(field) {
-        return row[field.name].value;
+    element.innerHTML = '<div id="main" style="width: 600px;height:400px;"></div>';
+
+    // Load ECharts library
+    var script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/echarts@5.4.2/dist/echarts.min.js';
+    script.onload = function() {
+      // Initialize the ECharts instance based on the prepared DOM
+      var myChart = echarts.init(document.getElementById('main'));
+
+      // Fetch data from Looker API
+      looker.plugins.visualizations.dataSingleQuery({
+        format: 'json', // Set the desired format
+        query: 'SELECT hour, day, value FROM your_view', // Replace with your Looker query
+        // You can pass additional parameters like filters, dimensions, or measures as required
+        // filters: { ... },
+        // dimensions: { ... },
+        // measures: { ... },
+        // ...
+      }).then(function(data) {
+        // Process the data returned from the API into the format required by the chart
+        var chartData = data.map(function(item) {
+          return [item.hour.value, item.day.value, item.value.value || '-'];
+        });
+
+        // Specify the configuration items and data for the chart
+        var option = {
+          tooltip: {
+            position: 'top'
+          },
+          grid: {
+            height: '50%',
+            top: '10%'
+          },
+          xAxis: {
+            type: 'category',
+            data: hours,
+            splitArea: {
+              show: true
+            }
+          },
+          yAxis: {
+            type: 'category',
+            data: days,
+            splitArea: {
+              show: true
+            }
+          },
+          visualMap: {
+            min: 0,
+            max: 10,
+            calculable: true,
+            orient: 'horizontal',
+            left: 'center',
+            bottom: '15%'
+          },
+          series: [
+            {
+              name: 'Punch Card',
+              type: 'heatmap',
+              data: chartData,
+              label: {
+                show: true
+              },
+              emphasis: {
+                itemStyle: {
+                  shadowBlur: 10,
+                  shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+              }
+            }
+          ]
+        };
+
+        // Display the chart using the configuration items and data
+        myChart.setOption(option);
       });
-    });
+    };
 
-    // Add your visualization code here using the retrieved data
-    // ...
-
-    done();
+    // Append the ECharts script to the DOM
+    document.head.appendChild(script);
   }
 });
