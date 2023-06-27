b6ef8f2ed1279b4cc56d56983218a5c6e856a74d
Natan Bond <97616282+NatanBondarenko@users.noreply.github.com>
2023-06-27 12:09:01 +0300

    Update Heatmap.js

--- Heatmap.js
+++ Heatmap.js
@@ -25,92 +25,95 @@ looker.plugins.visualizations.add({
     container.style.height = "100%";
   },
 
-  updateAsync: function(data, element, config, queryResponse, details, done) {
-    // Extract the dimensions and measures from the Looker query response
-    var dimensions = queryResponse.fields.dimension_like;
-    var measures = queryResponse.fields.measure_like;
+updateAsync: function(data, element, config, queryResponse, details, done) {
+  // Extract the dimensions and measures from the Looker query response
+  var dimensions = queryResponse.fields.dimension_like;
+  var measures = queryResponse.fields.measure_like;
 
-    // Prepare the x-axis data from the first dimension
-    var xAxisData = data.map(function(row) {
-      return row[dimensions[0].name].value;
-    });
+  // Prepare the x-axis and y-axis data from the dimensions
+  var xAxisData = data.map(function(row) {
+    return row[dimensions[0].name].value;
+  });
 
-    // Prepare the y-axis data from the second dimension
-    var yAxisData = data.map(function(row) {
-      return row[dimensions[1].name].value;
-    });
+  var yAxisData = data.map(function(row) {
+    return row[dimensions[1].name].value;
+  });
 
-    // Prepare the series data from the measure
-    var seriesData = data.map(function(row) {
-      return row[measures[0].name].value;
-    });
+  // Prepare the series data as a matrix
+  var seriesData = data.map(function(row) {
+    return [row[dimensions[0].name].value, row[dimensions[1].name].value, row[measures[0].name].value];
+  });
 
-    // Get the configured chart title and show label option
-    var chartTitle = config.title;
-    var showLabel = config.showLabel;
-    var labelFontSize = config.labelFontSize;
+  // Get the configured chart title, label options, and color options
+  var chartTitle = config.title;
+  var showLabel = config.showLabel;
+  var labelPosition = config.labelPosition;
+  var labelFontSize = config.labelFontSize;
+  var colorScheme = config.colorScheme || "heatmap";
 
-// Specify the configuration items for the chart
-var option = {
-  title: {
-    text: chartTitle
-  },
-  tooltip: {
-    formatter: function(params) {
-      return (
-        xAxisData[params.data[0]] +
-        " - " +
-        yAxisData[params.data[1]] +
-        ": " +
-        params.data[2]
-      );
-    }
-  },
-  xAxis: {
-    type: "category",
-    data: xAxisData
-  },
-  yAxis: {
-    type: "category",
-    data: yAxisData
-  },
-  visualMap: {
-    min: Math.min(...seriesData),
-    max: Math.max(...seriesData),
-    calculable: true,
-    orient: "horizontal",
-    left: "center",
-    bottom: 10
-  },
-  series: [
-    {
+  // Specify the configuration items for the chart
+  var option = {
+    title: {
+      text: chartTitle
+    },
+    tooltip: {
+      position: "top"
+    },
+    grid: {
+      top: 80,
+      bottom: 30,
+      left: 100
+    },
+    xAxis: {
+      type: "category",
+      data: xAxisData
+    },
+    yAxis: {
+      type: "category",
+      data: yAxisData
+    },
+    visualMap: {
+      min: Math.min(...seriesData.map(function(item) { return item[2]; })),
+      max: Math.max(...seriesData.map(function(item) { return item[2]; })),
+      calculable: true,
+      orient: "vertical",
+      left: 10,
+      top: "middle"
+    },
+    series: [{
       name: measures[0].label,
       type: "heatmap",
       data: seriesData,
       label: {
-        show: true,
-        color: "black",
-        fontSize: labelFontSize,
-        formatter: function(params) {
-          return params.value[2];
+        show: showLabel,
+        position: labelPosition,
+        textStyle: {
+          fontSize: labelFontSize
+        }
+      },
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowColor: "rgba(0, 0, 0, 0.5)"
         }
       }
-    }
-  ]
-};
+    }],
+    color: colorScheme
+  };
 
-    // Initialize the echarts instance based on the container element
-    var myChart = echarts.init(document.getElementById("main"));
+  // Initialize the echarts instance based on the container element
+  var myChart = echarts.init(document.getElementById("main"));
 
-    // Set responsive configuration
-    window.addEventListener("resize", function() {
-      myChart.resize();
-    });
+  // Set responsive configuration
+  window.addEventListener("resize", function() {
+    myChart.resize();
+  });
 
-    // Update the chart with the new configuration and data
-    myChart.setOption(option);
+  // Update the chart with the new configuration and data
+  myChart.setOption(option);
 
-    // Signal to Looker that the update is complete
-    done();
+  // Signal to Looker that the update is complete
+  done();
+
   }
 });
